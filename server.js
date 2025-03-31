a8de44fc4c024805ea260b4f482063bc53faeee1
harshadiwan <7khushidiwan@gmail.com>
2025-03-31 21:06:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use(express.urlencoded({extended:true}));
 // ✅ MySQL Connection
 const db = mysql.createConnection({
     host: "sql12.freesqldatabase.com",
